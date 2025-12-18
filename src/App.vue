b5badea7fd5b0141029bb7315a6d9ec60fa3decb
DESKTOP-BTDJ0S7\Administrator <enwls5626@gmail.com>
2025-12-18 21:25:53 +0900

    nav 삭제

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import AppNavBar from '@/components/common/AppNavBar.vue'
+import { RouterLink, RouterView } from "vue-router";
+import AppNavBar from "@/components/common/AppNavBar.vue";
 </script>
 
 <template>
   <header>
     <AppNavBar />
-    <!-- <nav>
-      <RouterLink :to="{ name: 'pills_index' }">영양제 목록</RouterLink> |
-      <RouterLink :to="{ name: 'login' }">로그인</RouterLink> |
-      <RouterLink :to="{ name: 'signup' }">회원가입</RouterLink> 
-    </nav> -->
   </header>
 
   <main>
@@ -19,20 +14,20 @@ import AppNavBar from '@/components/common/AppNavBar.vue'
 </template>
 
 <style scoped>
-  nav {
-    padding: 20px;
-    border-bottom: 1px solid #ddd;
-    margin-bottom: 20px;
-  }
+nav {
+  padding: 20px;
+  border-bottom: 1px solid #ddd;
+  margin-bottom: 20px;
+}
 
-  nav a {
-    margin: 0 10px;
-    text-decoration: none;
-    color: #333;
-    font-weight: bold;
-  }
+nav a {
+  margin: 0 10px;
+  text-decoration: none;
+  color: #333;
+  font-weight: bold;
+}
 
-  nav a.router-link-active {
-    color: #42b983;
-  }
+nav a.router-link-active {
+  color: #42b983;
+}
 </style>
